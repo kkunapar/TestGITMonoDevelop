c1a7a32eacd6f2e46cd5a2775d92217b23e684e8
kkunapar <kalicharan.kunaparaju@berkadia.com>
2017-08-31 06:17:37 -0400

    Initial check-in of module MyGITTesting

---
+++ Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+namespace MyGITTesting
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase()
+		{
+
+			System.Console.WriteLine("My GIT Testing");
+		
+		}
+	}
+}
